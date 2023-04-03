9a2e3b2621aff65963dea15ce0cd0aef592f6730
gspeed0689 <6378547+gspeed0689@users.noreply.github.com>
2023-04-03 16:07:46 +0200

    Added dict_grouping to mycode.py
    Added dict_grouping func to mycode.py, with helper functions list_maker and avg_diff.

--- mycode.py
+++ mycode.py
@@ -11,3 +11,19 @@ def power(x, y):
     v = x ** y # v is return value
 
     return(v) # return our calculation
+
+def list_maker(y, z):
+    rv = [v for v in range(y, z)] # rv = return value
+    return(rv)
+
+def avg_diff(x, y, z):
+    s = sum([x, y, z]) # s = sum
+    a = s / 3 # average
+    rv = x - a # rv = return value
+    return(rv)
+
+def dict_grouping(x, y, z):
+    d = {} # dictionary
+    for i in range(x):
+        d[i] = [list_maker(y, z), avg_diff(i, y, z)]
+    return(d)
